99360df07d579c78f3d7f085839c55ec88bab65e
Rajeev Patil <rajeevpatil24@gmail.com>
2026-06-14 23:15:42 +0530

    Fix GitHub Pages base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/rajeevpatil.github.io",
+  assetPrefix: "/rajeevpatil.github.io",
 };
 
 export default nextConfig;
