b20c8052b6b1151a047787f41124bcd39e78f856
Rajeev Patil <rajeevpatil24@gmail.com>
2026-06-14 23:45:13 +0530

    fix: css not loading on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+
+  // IMPORTANT FIX
+  assetPrefix: isProd ? "" : "",
+  basePath: isProd ? "" : ""
 };
 
 export default nextConfig;
